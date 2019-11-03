bdba70c59b29afb3e410071234812af138333443
Danil Sergiychuk <s99danil@gmail.com>
2019-11-03 17:33:35 +0200

    Second commit

--- projects/01_First/my-app/src/App.js
+++ projects/01_First/my-app/src/App.js
@@ -7,9 +7,10 @@ const App = () => {
     <div>
       <header>
         <img src='https://png.pngtree.com/element_our/sm/20180518/sm_5aff6089d3e02.png' />
-      <a>
-        <li>Hallo</li>
-      </a>
+      <div>
+        <li><a src="https://gitforwindows.org/">Hallo</a></li>
+        <li><a>End</a></li>
+      </div>
       
       </header>
     </div>
