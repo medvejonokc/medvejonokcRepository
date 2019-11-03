a8fe3b2d2e358aae53eb70e1dd833950bea55c4d
Danil Sergiychuk <s99danil@gmail.com>
2019-11-03 17:35:58 +0200

    Third commit

--- projects/01_First/my-app/src/App.js
+++ projects/01_First/my-app/src/App.js
@@ -8,8 +8,7 @@ const App = () => {
       <header>
         <img src='https://png.pngtree.com/element_our/sm/20180518/sm_5aff6089d3e02.png' />
       <div>
-        <li><a src="https://gitforwindows.org/">Hallo</a></li>
-        <li><a>End</a></li>
+        Third commit
       </div>
       
       </header>
