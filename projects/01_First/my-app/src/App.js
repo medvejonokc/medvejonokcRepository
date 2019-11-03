b919896bfac597c7f8be03bf53d15befabb79f1c
Danil Sergiychuk <s99danil@gmail.com>
2019-11-03 17:24:33 +0200

    first commit

---
+++ projects/01_First/my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <img src='https://png.pngtree.com/element_our/sm/20180518/sm_5aff6089d3e02.png' />
+      <a>
+        <li>Hallo</li>
+      </a>
+      
+      </header>
+    </div>
+  );
+}
+
+
+
+
+export default App;
